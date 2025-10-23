6868bdb57ab854df1c75fbf7df05c7c173537dae
SmartNine <realsmartnine@gmail.com>
2025-10-23 15:23:45 -0700

    [Description] 配置oss以https调用资源 [Issue #]

--- signleader-backend/services/ossClient.js
+++ signleader-backend/services/ossClient.js
@@ -1,11 +1,12 @@
-const OSS = require('ali-oss');
-const config = require('../config').oss;
+const OSS = require("ali-oss");
+const config = require("../config").oss;
 
 const client = new OSS({
   region: config.region,
   accessKeyId: config.accessKeyId,
   accessKeySecret: config.accessKeySecret,
-  bucket: config.bucket
+  bucket: config.bucket,
+  secure: config.secure,
 });
 
 module.exports = client;
